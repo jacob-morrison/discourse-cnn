60cf5edeb5f1f1a87864fcc8bcb107b4cd6666af
Jacob Morrison <Jacob@D-173-250-147-18.dhcp4.washington.edu>
2017-03-30 12:36:25 -0700

    fixed whitespacing :(

--- data_helpers.py
+++ data_helpers.py
@@ -7,7 +7,7 @@ from pprint import pprint
 def load_labels_and_data(model_file, data_file, smallSentences=False):
 	labels = {}
 	print "Loading model"
-        model = gs.models.KeyedVectors.load_word2vec_format(model_file, binary=True)
+	model = gs.models.KeyedVectors.load_word2vec_format(model_file, binary=True)
 	#model = gs.models.Word2Vec.load_word2vec_format(model_file, binary=True)
 	print "Model loaded"
 	# default these to the most popular sub-categories
@@ -51,57 +51,55 @@ def load_labels_and_data(model_file, data_file, smallSentences=False):
 				lab_vec[labels[(strs[0], strs[1])]] = 1
 				#ret_labels.append(labels[(strs[0], strs[1])])
 				ret_labels.append(lab_vec)
-                                sentence1 = pad_or_cut(tokens[24])
-                                sentence2 = pad_or_cut(tokens[34])
-                                bigSen1, smallSen1 = get_sentence_matrix(sentence1, model)
-                                bigSen2, smallSen2 = get_sentence_matrix(sentence2, model)
-                                if (smallSentences):
-                                        sentences1.append(smallSen1)
-                                        sentences2.append(smallSen2)
-                                else:
-                                        sentences1.append(bigSen1)
-                                        sentences2.append(bigSen2)
-			#sentences1.append(get_sentence_matrix(sentence1, model))
-			#sentences2.append(get_sentence_matrix(sentence2, model))
-        total = len(ret_labels)
-        ls = Counter()
-        for l in ret_labels:
-                idx = np.argmax(l)
-                ls[idx] += 1
-        mx = 0
-        for i in range(15):
-                if ls[i] > mx:
-                        mx = ls[i]
-        print("Accuracy if we did nothing: " + str(float(mx)/total))
+				sentence1 = pad_or_cut(tokens[24])
+				sentence2 = pad_or_cut(tokens[34])
+				bigSen1, smallSen1 = get_sentence_matrix(sentence1, model)
+				bigSen2, smallSen2 = get_sentence_matrix(sentence2, model)
+				if (smallSentences):
+					sentences1.append(smallSen1)
+					sentences2.append(smallSen2)
+				else:
+					sentences1.append(bigSen1)
+					sentences2.append(bigSen2)
+		total = len(ret_labels)
+		ls = Counter()
+		for l in ret_labels:
+			idx = np.argmax(l)
+			ls[idx] += 1
+		mx = 0
+		for i in range(15):
+			if ls[i] > mx:
+				mx = ls[i]
+		print("Accuracy if we did nothing: " + str(float(mx)/total))
 	return sentences1, sentences2, ret_labels
 
 def test(data_file):
-        total = 0
-        total50 = 0
-        total75 = 0
-        total100 = 0
-        with open(data_file) as f:
-                for line in f:
-                        total += 2
-                        tokens = line.split("|")
-                        sen1 = tokens[24].split()
-                        sen2 = tokens[34].split()
-                        if len(sen1) > 50:
-                                total50 += 1
-                        if len(sen1) > 75:
-                                total75 += 1
-                        if len(sen1) > 100:
-                                total100 += 1
-                        if len(sen2) > 50:
-                                total50 += 1
-                        if len(sen2) > 75:
-                                total75 += 1
-                        if len(sen2) > 100:
-                                total100 += 1
-        print(str(total) + " " + str(total50) + " " + str(total75) + " " + str(total100))
+	total = 0
+	total50 = 0
+	total75 = 0
+	total100 = 0
+	with open(data_file) as f:
+		for line in f:
+			total += 2
+			tokens = line.split("|")
+			sen1 = tokens[24].split()
+			sen2 = tokens[34].split()
+			if len(sen1) > 50:
+				total50 += 1
+			if len(sen1) > 75:
+				total75 += 1
+			if len(sen1) > 100:
+				total100 += 1
+			if len(sen2) > 50:
+				total50 += 1
+			if len(sen2) > 75:
+				total75 += 1
+			if len(sen2) > 100:
+				total100 += 1
+	print(str(total) + " " + str(total50) + " " + str(total75) + " " + str(total100))
 
 def pad_or_cut(sen):
-        sen_len = 75
+	sen_len = 75
 	words = sen.split(" ")
 	l = len(words)
 	if l > sen_len:
@@ -121,7 +119,7 @@ def get_sentence_matrix(sentence, model):
 			mat = np.column_stack([mat, model[word]])
 		except:
 			mat = np.column_stack([mat, np.zeros(300, dtype=float)])
-        return mat, np.mean(mat, axis=1)
+	return mat, np.mean(mat, axis=1)
 
 
 if __name__ == '__main__':
