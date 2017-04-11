5427cffb3c91f0ccfe1594cbe37f2164262695ca
Jacob Morrison <Jacob@D-69-91-183-250.dhcp4.washington.edu>
2017-04-11 13:13:18 -0700

    trying new learning parameters

--- data_helpers.py
+++ data_helpers.py
@@ -165,8 +165,10 @@ def pad_or_cut(sen):
 	#sen = sen.replace('"', ' ')
 	#sen = sen.replace(',', ' ')
 	#sen = sen.replace('-', ' ')
-	sen = sen.replace('$', ' ')
+	#sen = sen.replace('$', ' ')
 	#sen = sen.replace('.', ' ')
+	sen = sen.replace('0', ' 0 ').replace('1', ' 1 ').replace('2', ' 2 ').replace('3', ' 3 ').replace('4', ' 4 ')
+	sen = sen.replace('5', ' 5 ').replace('6', ' 6 ').replace('7', ' 7 ').replace('8', ' 8 ').replace('9', ' 9 ')
 	words = sen.split()
 	l = len(words)
 	if l > sen_len:
