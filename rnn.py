313aa364ebf2fab142b6d70110e3500d657623a9
Jacob Morrison <Jacob@D-173-250-185-33.dhcp4.washington.edu>
2017-04-14 14:16:11 -0700

    moving new rnn to zin

--- rnn.py
+++ rnn.py
@@ -1,6 +1,7 @@
 # Jacob Morrison
 
 import tensorflow as tf
+from tf.nn import rnn
 import numpy as np
 import data_helpers
 import os
@@ -14,14 +15,14 @@ batch_size = 128
 display_step = 10
 
 # network parameters
-n_input = 75 # truncate sentences (pad sentences with <PAD> tokens if less than this, cut off if larger)
-n_steps = 300
+n_words = 75 # truncate sentences (pad sentences with <PAD> tokens if less than this, cut off if larger)
+n_dim = 300
 n_hidden = 128 # hidden layer num of features
 n_classes = 16 # 15 total senses
 
 # tf graph input
-x1 = tf.placeholder(tf.float32, [None, n_steps, n_input])
-x2 = tf.placeholder(tf.float32, [None, n_steps, n_input])
+x1 = tf.placeholder(tf.float32, [None, n_dim, n_words])
+x2 = tf.placeholder(tf.float32, [None, n_dim, n_words])
 y = tf.placeholder(tf.float32, [None, n_classes])
 
 # Define weights
@@ -32,49 +33,37 @@ biases = {
     'out': tf.Variable(tf.random_normal([n_classes]))
 }
 
-def RNN(x1, x2):
+def RNN(x):
+    # split input into n_words [batch_size, n_dim] tensors
+    x = tf.unstack(x, n_words, 2)
 
-    # Prepare data shape to match `rnn` function requirements
-    # Current data input shape: (batch_size, n_steps, n_input)
-    # Required shape: 'n_steps' tensors list of shape (batch_size, n_input)
-    
-    # Permuting batch_size and n_steps
-    x1 = tf.transpose(x1, [1, 0, 2])
-    x2 = tf.transpose(x2, [1, 0, 2])
-    # Reshaping to (n_steps*batch_size, n_input)
-    x1 = tf.reshape(x1, [-1, n_steps])
-    x2 = tf.reshape(x2, [-1, n_steps])
-    # Split to get a list of 'n_steps' tensors of shape (batch_size, n_input)
-    x1 = tf.split(0, n_input, x1)
-    x2 = tf.split(0, n_input, x2)
+    # define an LSTM cell
+    lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(n_hidden, forget_bias=1.0)
 
-    # Define a lstm cell with tensorflow
-    with tf.variable_scope('lstm1'):
-        lstm_cell1 = tf.nn.rnn_cell.BasicLSTMCell(n_hidden, forget_bias=1.0)
-        outputs1, states1 = tf.nn.rnn(lstm_cell1, x1, dtype=tf.float32)
-    with tf.variable_scope('lstm2'):
-        lstm_cell2 = tf.nn.rnn_cell.BasicLSTMCell(n_hidden, forget_bias=1.0)
-        outputs2, states2 = tf.nn.rnn(lstm_cell2, x2, dtype=tf.float32)
+    # get the output of the cell
+    outputs, states = rnn.rnn(lstm_cell, x, dtype=tf.float32)
 
-    return tf.concat(1, [outputs1[-1], outputs2[-1]])
+    # return last output from cell
+    return outputs[-1]
+
+
 
 # need to get a prediction for each sentence
 
 # get the vector representation of each word
-#pred1 = RNN(x1)
-#pred2 = pred1
-#pred2 = RNN(x2)
+pred1 = RNN(x1)
+pred2 = RNN(x2)
 
-# concatenate both representations
-#out = tf.concat(1, [pred1, pred2])
-out = RNN(x1, x2)
+# do something with both representations
+# simple concatenation?
+out = tf.concat(1, [pred1, pred2])
 
 # predict the relation class
-pred = tf.add(tf.batch_matmul(out, weights['out']), biases['out'])
+pred = tf.add(tf.matmul(out, weights['out']), biases['out'])
 
 # define loss and optimizer
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y))
-optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(cost)
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 
 # Evaluate model
 correct_pred = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
@@ -89,82 +78,65 @@ tf.add_to_collection('accuracy', accuracy)
 tf.add_to_collection('x1', x1)
 tf.add_to_collection('x2', x2)
 tf.add_to_collection('y', y)
+
 with tf.Session() as sess:
-	sess.run(init)
-	step = 1
-	sentences1, sentences2, labels = data_helpers.load_labels_and_data('./Data/GoogleNews-vectors-negative300.bin', './Data/implicitTrainPDTB.txt')
-	# keep training until we reach max iterations
-        #print(len(sentences1))
-        total = 0
-	while total < training_iters:
-		start = total  % len(sentences1)
-		end = (total + batch_size) % len(sentences1)
-		if end <= start:
-			end = len(sentences1)
-		batch_x1 = sentences1[start : end]
-		batch_x2 = sentences2[start : end]
-		batch_y = labels[start : end]
-		total += (len(batch_x1))
-		sess.run(optimizer, feed_dict={x1: batch_x1, x2: batch_x2, y: batch_y})
-		if step % display_step == 0:
-			#calculate batch loss and accuracy
-			loss, acc = sess.run([cost, accuracy], feed_dict={x1: batch_x1, x2: batch_x2, y: batch_y})
-			print "Iter " + str(total) + ", Minibatch Loss= " + \
-                  "{:.6f}".format(loss) + ", Training Accuracy= " + \
-                  "{:.5f}".format(acc)
+    sess.run(init)
+    step = 1
+    model = data_helpers.load_model('./Data/GoogleNews-vectors-negative300.bin')
+    sentences1, sentences2, labels = data_helpers.load_labels_and_data(model, './Data/implicitTrainPDTB.txt')
+    total = 0
 
-		step += 1
+    while total < training_iters:
+        start = total  % len(sentences1)
+        end = (total + batch_size) % len(sentences1)
+        if end <= start:
+            end = len(sentences1)
+        batch_x1 = sentences1[start : end]
+        batch_x2 = sentences2[start : end]
+        batch_y = labels[start : end]
+        total += (len(batch_x1))
+        sess.run(optimizer, feed_dict={x1: batch_x1, x2: batch_x2, y: batch_y})
+        if step % display_step == 0:
+            #calculate batch loss and accuracy
+            loss, acc = sess.run([cost, accuracy], feed_dict={x1: batch_x1, x2: batch_x2, y: batch_y})
+            print "Iter " + str(total) + ", Minibatch Loss= " + \
+                "{:.6f}".format(loss) + ", Training Accuracy= " + \
+                "{:.5f}".format(acc)
+            # extract variables here
+            #w2 = sess.run(weights['w2'])
+            #print(w2)
+        step += 1
+    print "Training finished!"
 
-	print "Training finished!"
+    # calculate training set accuracy
+    print("testing accuracy on training set: ")
+    step = 0
+    acc = 0.
+    print(len(sentences1))
+    batch_size2 = batch_size * 2
+    while step * batch_size2 < len(sentences1):
+        start = (step * batch_size2)
+        end = ((step + 1) * batch_size2)
+        if end > len(sentences1):
+            end = len(sentences1)
+        batch_x1 = sentences1[start : end]
+        batch_x2 = sentences2[start : end]
+        batch_y = labels[start : end]
+        acc += (float(len(batch_x1)) / len(sentences1)) * sess.run(accuracy, feed_dict={x1: batch_x1, x2: batch_x2, y: batch_y})
+        step += 1
+    print(str(acc))
 
-	#then save model
-        timestamp = str(int(time.time()))
-	out_dir = os.path.abspath(os.path.join(os.path.curdir, "runs", timestamp))
-    # Checkpoint directory. Tensorflow assumes this directory already exists so we need to create it
-	checkpoint_dir = os.path.abspath(os.path.join(out_dir, "checkpoints"))
-	checkpoint_prefix = os.path.join(checkpoint_dir, "model")
-	if not os.path.exists(checkpoint_dir):
-		os.makedirs(checkpoint_dir)
-	path = saver.save(sess, checkpoint_prefix, global_step=1)
-	print("Saved model checkpoint to {}\n".format(path))
+    # test accuracy on dev set
+    print("accuracy on dev set:")
+    sentences12, sentences22, labels2 = data_helpers.load_labels_and_data(\
+        model, \
+        './Data/devImplicitPDTB.txt')                          
+    print(str(sess.run(accuracy, feed_dict={x1: sentences12, x2: sentences22, y: labels2})))
 
-        print("testing accuracy on training set: ")#
-        step = 0
-        acc = 0.
-        #sentences1, sentences2, labels = data_helpers.load_labels_and_data('./Data/GoogleNews-vectors-negative300.bin', './Data/implicitTrainPDTB.txt')
-        print(len(sentences1))
-        batch_size2 = batch_size * 2
-        while step * batch_size2 < len(sentences1):
-                start = (step * batch_size2)
-                end = ((step + 1) * batch_size2)
-                if end > len(sentences1):
-                        end = len(sentences1)
-                batch_x1 = sentences1[start : end]
-                batch_x2 = sentences2[start : end]
-                batch_y = labels[start : end]
-                acc += (float(len(batch_x1)) / len(sentences1)) * sess.run(accuracy, feed_dict={x1: batch_x1, x2: batch_x2, y: batch_y})
-                step += 1
-        print(str(acc))
-        #print(str(sess.run(accuracy, feed_dict={x1: sentences1, x2: sentences2, y: labels, keep_prob: 1.})))
-        sentences12, sentences22, labels2 = data_helpers.load_labels_and_data('./Data/GoogleNews-vectors-negative300.bin', './Data/devImplicitPDTB.txt')                          
+    # test accuracy on dev set
+    print("accuracy on test set:")
+    sentences12, sentences22, labels2 = data_helpers.load_labels_and_data(\
+        model, \
+        './Data/testImplicitPDTB.txt')                          
+    print(str(sess.run(accuracy, feed_dict={x1: sentences12, x2: sentences22, y: labels2})))
 
-        print(len(sentences12))
-        batch_size2 = batch_size * 2
-        step = 0
-        acc = 0.
-        while step * batch_size2 < len(sentences12):
-                start = (step * batch_size2)
-                end = ((step + 1) * batch_size2)
-                if end > len(sentences12):
-                        end = len(sentences12)
-                if start >= len(sentences12):
-                        break;
-                batch_x1 = sentences12[start : end]
-                batch_x2 = sentences22[start : end]
-                batch_y = labels2[start : end]
-                acc += (float(len(batch_x1)) / len(sentences12)) * sess.run(accuracy, feed_dict={x1: batch_x1, x2: batch_x2, y: batch_y})
-                step += 1
-        #print(str(acc/(len(sentences1)/batch_size)))
-        print(str(acc))
-        
-        print(str(sess.run(accuracy, feed_dict={x1: sentences12, x2: sentences22, y: labels2})))
