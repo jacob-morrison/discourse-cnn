09cb47b1cd9a77a3712acfb95324f3ce7306b495
Jacob Morrison <Jacob@D-173-250-162-216.dhcp4.washington.edu>
2017-04-18 13:46:48 -0700

    testing change to rnn on zin

--- rnn.py
+++ rnn.py
@@ -59,11 +59,11 @@ lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(n_hidden, forget_bias=1.0)
 
 # get the vector representation of each word
 with tf.variable_scope('scope1') as scope1:
-    pred1 = Dynamic_RNN(tf.transpose(x1, perm=[0, 2, 1]), lstm_cell, x1_len)#, [-1, n_hidden]
+    pred1 = tf.reshape(Dynamic_RNN(tf.transpose(x1, perm=[0, 2, 1]), lstm_cell, x1_len), [-1, n_hidden])
     #pred1 = RNN(x1, lstm_cell)
 with tf.variable_scope('scope1') as scope1:
     scope1.reuse_variables()
-    pred2 = Dynamic_RNN(tf.transpose(x2, perm=[0, 2, 1]), lstm_cell, x2_len)
+    pred2 = tf.reshape(Dynamic_RNN(tf.transpose(x2, perm=[0, 2, 1]), lstm_cell, x2_len), [-1, n_hidden])
     #pred2 = RNN(x2, lstm_cell)
 
 # do something with both representations
