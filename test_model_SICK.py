8b2ac4381b8ffc7f66247ef6b6e78290f6fe5965
Jacob Morrison <Jacob@D-173-250-189-157.dhcp4.washington.edu>
2017-04-19 14:10:04 -0700

    adding proof of concept SICK model

--- test_model_SICK.py
+++ test_model_SICK.py
@@ -64,7 +64,7 @@ with tf.Session() as sess:
 	sess.run(init)
 	step = 1
 	model = data_helpers.load_model('./Data/GoogleNews-vectors-negative300.bin')
-	sentences1, sentences2, labels = data_helpers.load_labels_and_data_PDTB(model, './Data/SICK/train.txt')
+	sentences1, sentences2, labels = data_helpers.load_data_SICK(model, './Data/SICK/train.txt')
 	total = 0
 
 	while total < training_iters:
@@ -109,7 +109,7 @@ with tf.Session() as sess:
 
 	# test accuracy on dev set
 	print("accuracy on dev set:")
-	sentences12, sentences22, labels2 = data_helpers.load_labels_and_data_PDTB(\
+	sentences12, sentences22, labels2 = data_helpers.load_data_SICK(\
 		model, \
 		'./Data/SICK/dev.txt')                          
 	print(str(sess.run(accuracy, feed_dict={x1: sentences12, x2: sentences22, y: labels2})))
